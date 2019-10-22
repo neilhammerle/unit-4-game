6b318ca114377c6cc12e09dcac108e6cd40162f3
Neil Hammerle <neilhammerle@Neils-MacBook-Pro.local>
2019-10-22 02:41:00 -0700

    inter files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    crystals = ['assets/images/Blue-Crystal.png', 'assets/images/Purple-Crystal.jpg', 'assets/images/Red-Crystal.jpg', 'assets/images/Yellow-Crystal.jpg'];
     var counter = 0;
     var wins = 0;
     var losses = 0;
@@ -20,6 +21,37 @@ $(document).ready(function() {
         }
         if (!found)numbers[numbers.length]= randomNumber;
     }
+    for (i = 0; i < numbers.length; i++) {
+        var imageCrytal = $('<img>');
+        imageCrytal.attr('data-num', numbers [i]);
+        imageCrytal.attr('src', crytals[i]);
+        imageCrytal.attr('alt', 'crytals');
+        imageCrytal.addClass('crstalImage')
+        $('#crystals').append(imageCrytal);
+    }
+    function newGame() {
 
+        counter = 0;
+        $ ('#yourScore').text(counter);
 
+        function randomIntFromInterval();
+
+        $('value').text(numberToGuess);
+
+        $('.crystalImage').on('click', function(){
+            counter = counter + parseInt($(this).data('num'));
+
+            $('#yourScore').text(counter);
+
+            if (counter == numberToGuess){
+                $('#status').text('You Lost!');
+                losses ++;
+                $('#loss').text(losses);
+                console,log(losses)
+                $('#crystals').empty();
+                newCrystals();
+                newGame();
+            }
+        });
+    }
 });
